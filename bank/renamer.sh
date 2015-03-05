b5b9d0376bbcb9bc253296a7d1face856f1a7909
Henrik Steen <henrist@henrist.net>
2015-03-05 22:40:40 +0100

    Fix date parsing of KID. Fix some whitespace.

--- bank/renamer.sh
+++ bank/renamer.sh
@@ -86,13 +86,13 @@ function copytoprint() {
 	# bygg opp nytt filnavn og sjekk om denne finnes
 	# hvis den finnes, finn et annet navn
 	printname="$DIR_UTSKRIFT/$destname.pdf"
-	
+
 	local i=2
 	while [[ -e "$printname" ]]; do
 		printname="$destpath/$destname ($i).pdf"
 		let i=i+1
 	done
-	
+
 	if [[ $ISTEST == "1" ]]; then
 		echo -e "${GREEN2}   $printname${NC}"
 	else
@@ -106,7 +106,7 @@ function movetodest() {
 	# bygg opp nytt filnavn og sjekk om denne finnes
 	# hvis den finnes, finn et annet navn
 	destfile="$destpath/$destname.pdf"
-	
+
 	local i=2
 	exists=0
 	while [[ -e "$destfile" ]]; do
@@ -122,14 +122,14 @@ function movetodest() {
 		destfile="$destpath/$destname ($i).pdf"
 		let i=i+1
 	done
-	
+
 	if [[ $ISTEST == "1" ]]; then
 		echo -e "${GREEN}   $destfile${NC}"
 	else
 		mv "$filepath" "$destfile"
 		echo -e "${GREEN}   $destfile${NC}"
 	fi
-	
+
 	if [[ $exists == "0" ]]; then
 		copytoprint
 	fi
@@ -141,7 +141,7 @@ function extract_date_kreditering_normal() {
 	while true; do
 		let n=56+$x
 		line=$(echo "$filedata" | sed "${n}q;d")
-		
+
 		if [[ ! $line =~ ^[0-9]{2}/[0-9]{2}-[0-9]{4}$ ]]; then
 			if (( x > 7 )); then
 				return
@@ -149,30 +149,19 @@ function extract_date_kreditering_normal() {
 			let x=$x+1
 			continue
 		fi
-		
+
 		date=${line:6:4}-${line:3:2}-${line:0:2}
 		break
 	done
 }
 
 function extract_date_kreditering_kid() {
+	line=$(echo "$filedata" | grep -A1 "VAL.DATO" | tail -n 1)
+
 	date=""
-	local x=0
-	while true; do
-		let n=25+$x
-		line=$(echo "$filedata" | sed "${n}q;d")
-		
-		if [[ ! $line =~ ^[0-9]{2}\.[0-9]{2}\.[0-9]{2}$ ]]; then
-			if (( x > 7 )); then
-				return
-			fi
-			let x=$x+1
-			continue
-		fi
-		
+	if [[ $line =~ ^[0-9]{2}\.[0-9]{2}\.[0-9]{2}$ ]]; then
 		date=20${line:6:2}-${line:3:2}-${line:0:2}
-		break
-	done
+	fi
 }
 
 function fixdir() {
@@ -192,7 +181,7 @@ function handlekreditering() {
 	if [[ $filedata != *"Denne forsendelsen"* ]]; then
 		ISKID=1
 	fi
-	
+
 	# trekk ut dato fra filen
 	if [[ $ISKID == "1" ]]; then
 		extract_date_kreditering_kid
@@ -203,7 +192,7 @@ function handlekreditering() {
 		echo -e "${RED}   Klarte ikke å hente ut dato${NC}"
 		return
 	fi
-	
+
 	# trekk ut til-konto fra filen
 	if [[ $ISKID == "1" ]]; then
 		konto=$(echo "$filedata" | grep "KTO" | head -n 1 | sed 's/^.*\(\([0-9]\)\{4\}\) \(\([0-9]\)\{2\}\) \(\([0-9]\)\{5\}\).*$/\1.\3.\5/')
@@ -215,11 +204,11 @@ function handlekreditering() {
 		return
 	fi
 	kortkonto=${konto:8}
-	
+
 	# kontroller at mappen for dennne filen finnes
 	destpath="$DIR_KREDITERINGER/${date:0:4}/$konto"
 	fixdir
-	
+
 	# flytt til ny mappe
 	destname="Innbetalinger $kortkonto $date"
 	movetodest
@@ -233,21 +222,21 @@ function handleutbetaling() {
 	belop=$(echo "$info" | awk 'NR==23')
 	dato=$(echo "$info" | awk 'NR==24' | awk 'BEGIN{FS=OFS="."}{print $3"-"$2"-"$1}')
 	year=$(echo "$dato" | sed 's/^\(....\)-.*$/\1/')
-	
+
 	if [[ ${#year} -ne 4 ]]; then
 		echo -e "${RED}   Klarte ikke å hente ut dato${NC}"
 		return
 	fi
-	
+
 	if [ "${#konto}" -ne "13" ]; then
 		echo -e "${RED}   Fant ikke kontonr${NC}"
 		return
 	fi
-	
+
 	# kontroller at mappen for dennne filen finnes
 	destpath="$DIR_UTBETALINGER/$year/$konto"
 	fixdir
-	
+
 	# flytt til ny mappe
 	destname="$dato $konto Betalt $mottaker ($belop)"
 	movetodest
@@ -256,44 +245,44 @@ function handleutbetaling() {
 function handlekontoutskrift() {
 	info=$(echo "$filedata" | sed '/^$/d')
 	date=$(echo "$info" | head -n 1 | sed 's/^\(..\).\(..\).\(....\)$/\3-\2-\1/')
-    
+
 	if [ ${#date} -ne "10" ]; then
 		echo -e "${RED}   Klarte ikke å hente ut dato${NC}"
 		return
-    fi
-	
+	fi
+
 	nr=$(echo "$info" | grep -A 1 -m 1 'UTSKRIFTSNR.' | tail -n 1)
-    re='^[0-9]+$'
-    if ! [[ $nr =~ $re ]]; then
+	re='^[0-9]+$'
+	if ! [[ $nr =~ $re ]]; then
 		echo -e "${RED}   Klarte ikke å hente ut utskriftsnummer${NC}"
 		return
-    fi
+	fi
 
-    konto=$(echo "$info" | grep -m 1 'KONTONR.' | sed 's/KONTONR\. //')
-    re='^[0-9]{4}\.[0-9]{2}\.[0-9]{5}$'
-    if ! [[ $konto =~ $re ]]; then
-        echo -e "${RED}   Klarte ikke å hente ut kontonr${NC}"
+	konto=$(echo "$info" | grep -m 1 'KONTONR.' | sed 's/KONTONR\. //')
+	re='^[0-9]{4}\.[0-9]{2}\.[0-9]{5}$'
+	if ! [[ $konto =~ $re ]]; then
+		echo -e "${RED}   Klarte ikke å hente ut kontonr${NC}"
 		return
-    fi
-	
+	fi
+
 	if ! ls "$DIR_KONTOUTSKRIFTER/"*$konto* 1>/dev/null 2>&1; then
 		echo -e "${RED}   Klarte ikke å identifisere mappen til kontoutskrift for konto $konto${NC}"
 		return
 	fi
-	
+
 	mappe=""
 	for y in "$DIR_KONTOUTSKRIFTER/"*$konto*; do
 		mappe=$y
-    done
-	
+	done
+
 	destpath="$mappe"
 	destname="Kontoutskrift ${konto:8:5} $date nr $nr"
-	
+
 	if [[ -e "$destpath/$destname.pdf" ]]; then
-        echo -e "${RED}   Kontoutskrift eksisterer allerede som $destname${NC}"
+		echo -e "${RED}   Kontoutskrift eksisterer allerede som $destname${NC}"
 		return
-    fi
-	
+	fi
+
 	movetodest
 }
 
@@ -301,9 +290,9 @@ function handlekontoutskrift() {
 function checkfiles() {
 	for filepath in "$DIR_INPUT/"*.pdf; do
 		filedata=$(pdftotext "$filepath" - 2>/dev/null)
-		
+
 		echo -e "${YELLOW}Fil: $filepath${NC}"
-		
+
 		if [[ $filedata == *"MELDING OM KREDITERING"* ]]; then
 			handlekreditering
 		elif [[ $filedata == *"Godskrift på mottakers konto"* ]]; then
