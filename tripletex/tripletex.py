cb459fdb207521f7b6c7c192bd3febd0427f84a4
Henrik Steen <henrist@henrist.net>
2015-11-05 18:17:58 +0100

    Automatisk generer alle rapporter fra Tripletex til budsjettarket

--- tripletex/tripletex.py
+++ tripletex/tripletex.py
@@ -8,6 +8,8 @@ import http
 import urllib.parse
 import re
 import time
+import sys
+import os.path
 from bs4 import BeautifulSoup
 from collections import OrderedDict
 
@@ -164,7 +166,7 @@ class Tripletex(TripletexBase):
                     "javaClass": "java.util.Date",
                     "time": '1446332400000'
                 },
-                ""
+                ''
             ]
         }
 
@@ -300,7 +302,7 @@ class Tripletex(TripletexBase):
         q['csvHeader'] = "true"
         q['csvEncoding'] = "UTF-8"
         q['csvSeparator'] = ";"
-        q['csvQualifier'] = "\""
+        q['csvQualifier'] = '"'
         q['csvDecimal'] = "."
         q['csvLineBreak'] = "\n"
 
@@ -414,7 +416,7 @@ class Tripletex(TripletexBase):
                         "checksum": file_data['checksum'],
                         "clientId": "upload"
                     },
-                    "file": "",
+                    "file": '',
                     "generateVatPostings": True
                 },
                 "doImportVouchers"
@@ -462,54 +464,76 @@ class UploadFailedException(TripletexException):
     pass
 
 
-def print_accounts(tripletex):
+def get_accounts_list(tripletex):
     accounts = tripletex.get_accounts()
+    ret = ''
     for account in accounts:
-        print('%s\t%s\t%s\t%s' % (
+        ret += '%s\t%s\t%s\t%s\n' % (
             account['id'],
             account['text'],
             account['group'],
             int(account['active'])
-        ))
+        )
+    return ret
 
-def print_report_result(tripletex, year, sem):
+def get_accounts_report(tripletex, year, sem):
     project_number = 40041
 
     project_id = tripletex.get_project_id(project_number)
     result = tripletex.get_report_result(date_start=str(year)+sem['start'], date_end=str(year)+sem['end'], project_id=project_id)
 
+    ret = ''
     for account in result:
-        print('%s\t\t%s\t\t\t%s\t%s\tregnskap\t%s\t%s' % (project_number, account[0], year, sem['text'],
-                                                          time.strftime('%Y%m%d'), str(account[1]).replace('.', ',')))
+        ret += '%s\t\t%s\t\t\t%s\t%s\tregnskap\t%s\t%s\n' % (project_number, account[0], year, sem['text'],
+                                                          time.strftime('%Y%m%d'), str(account[1]).replace('.', ','))
+    return ret
 
-def print_report_projects(tripletex, year, sem):
+def get_projects_report(tripletex, year, sem):
     result = tripletex.get_report_projects(date_start=str(year)+sem['start'], date_end=str(year)+sem['end'])
 
+    ret = ''
     for project in result:
-        print('%s\t\t\t%s\t%s\tregnskap\t%s\t%s\t%s' % (project[0], year, sem['text'], time.strftime('%Y%m%d'),
+        ret += '%s\t\t\t%s\t%s\tregnskap\t%s\t%s\t%s\n' % (project[0], year, sem['text'], time.strftime('%Y%m%d'),
                                                       str(project[2]).replace('.', ','),
-                                                      str(project[3]).replace('.', ',')))
+                                                      str(project[3]).replace('.', ','))
+    return ret
 
-def print_project_list(tripletex):
+def get_projects_list(tripletex):
+    ret = ''
     projects = tripletex.get_project_list()
     for project in projects:
-        print('%s\t%s\t%s' % (project['parent'], project['number'], project['text']))
+        ret += '%s\t%s\t%s\n' % (project['parent'], project['number'], project['text'])
+    return ret
 
 if __name__ == '__main__':
     tt = Tripletex()
 
-    #print_accounts(tt)
-    #print_project_list(tt)
+    home = os.path.expanduser('~')
+    path = home + '/Dropbox/Økonomigruppa/8 Budsjett og regnskap (internt)/2016/'
 
-    print_report_result(tt, 2014, SEMESTERS[0])
-    print_report_result(tt, 2014, SEMESTERS[1])
-    print_report_result(tt, 2015, SEMESTERS[0])
-    print_report_result(tt, 2015, SEMESTERS[1])
+    with open(path + 'accounts_list.txt', 'w') as f:
+        f.write(get_accounts_list(tt))
+    print('Exported accounts list')
 
-    #print_report_projects(tt, 2014, SEMESTERS[0])
-    #print_report_projects(tt, 2014, SEMESTERS[1])
-    #print_report_projects(tt, 2015, SEMESTERS[0])
-    #print_report_projects(tt, 2015, SEMESTERS[1])
+    with open(path + 'projects_list.txt', 'w') as f:
+        f.write(get_projects_list(tt))
+    print('Exported projects list')
+
+    with open(path + 'accounts_report.txt', 'w') as f:
+        f.write(get_accounts_report(tt, 2014, SEMESTERS[0]))
+        f.write(get_accounts_report(tt, 2014, SEMESTERS[1]))
+        f.write(get_accounts_report(tt, 2015, SEMESTERS[0]))
+        f.write(get_accounts_report(tt, 2015, SEMESTERS[1]))
+    print('Exported accounts report')
+
+    with open(path + 'projects_report.txt', 'w') as f:
+        f.write(get_projects_report(tt, 2014, SEMESTERS[0]))
+        f.write(get_projects_report(tt, 2014, SEMESTERS[1]))
+        f.write(get_projects_report(tt, 2015, SEMESTERS[0]))
+        f.write(get_projects_report(tt, 2015, SEMESTERS[1]))
+    print('Exported projects report')
+
+    print('Reports saved to files in %s' % path)
 
     #print("Testing Tripletex-connection:")
     #num = tt.get_next_ledger_number(2015)
