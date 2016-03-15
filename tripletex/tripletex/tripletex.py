290149c99cf1d782a69f6790bb3bb0a2394bff06
Henrik Steen <henrist@henrist.net>
2016-03-15 19:51:47 +0100

    Fiks henting av prosjektliste etter nye innstillinger i Tripletex

--- tripletex/tripletex/tripletex.py
+++ tripletex/tripletex/tripletex.py
@@ -472,7 +472,7 @@ class TripletexProjects(TripletexBase):
         for tr in re.findall(r'<tr.*?>(.+?)</tr>', r.text):
             tdlist = re.findall(r'<td.*?>(.+?)</td>', tr)
 
-            if len(tdlist) == 7 or len(tdlist) == 8:  # 8 if avdeling is enabled/visible
+            if len(tdlist) > 7:
                 project_id = re.search(r'projectId=(\d+)&', tdlist[1]).group(1)
 
                 start_and_end = re.sub(r'<[^>]*?>', '', tdlist[5])
