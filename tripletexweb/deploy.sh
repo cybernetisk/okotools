7cae8f8cff76700933317c09dc91e93ef9dc253f
Henrik Steen <henrist@henrist.net>
2016-04-12 22:41:10 +0200

    Deploy-script

---
+++ tripletexweb/deploy.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# deploy script - deploys to internt.cyb.no/okoreports and foreningenbs.no/okoreports
+
+echo "Deploying to internt.cyb.no"
+ssh django@internt.cyb.no /bin/bash <<EOF
+  cd /home/django/okotools
+  git pull
+  cd tripletexweb/web
+  npm install
+  npm run build
+  cd ../../tripletex
+  . env/bin/activate
+  pip install -r requirements.txt
+EOF
+
+echo
+echo "Deploying to foreningenbs.no"
+ssh henrste@foreningenbs.no /bin/bash <<EOF
+  eval \$(ssh-agent)
+  ssh-add ~/.ssh/cyb_okotools
+  cd /var/www/aliases/okoreports/okotools
+  git pull
+  cd tripletexweb/web
+  npm install
+  npm run build
+  cd ../../tripletex
+  . env/bin/activate
+  pip install -r requirements.txt
+EOF
