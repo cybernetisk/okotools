5a92d0b332d401a9a03c5be76426d8d0377a0167
Henrik Steen <henrist@henrist.net>
2016-09-18 14:44:04 +0200

    Remove settings file

--- tripletexweb/backend/settings.py
+++
@@ -1,6 +0,0 @@
-contextId = 2845076
-credentials = ('okonomi@cyb.no', 'P0wderpuff')
-credentials_provider=lambda: credentials
-budget_url = 'https://spreadsheets.google.com/feeds/worksheets/1pAEq8O5NMkmEWvW-c6x_47abg5IO7HqPO5bs5J-iPt4/public/full?alt=json'
-budget_edit_url = 'https://docs.google.com/spreadsheets/d/1pAEq8O5NMkmEWvW-c6x_47abg5IO7HqPO5bs5J-iPt4/edit'
-reports_path = '/var/okoreports/reports/'
