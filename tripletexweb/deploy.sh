893d7316718f2c6183c217e41ee3e9c059338154
Henrik Steen <henrist@henrist.net>
2016-09-19 14:21:47 +0200

    Make deploy-script usable again

--- tripletexweb/deploy.sh
+++ tripletexweb/deploy.sh
@@ -1,33 +1,23 @@
 #!/bin/bash
 
-echo "Not updated for Docker-setup"
-exit 1
+# deploy script - deploys to okoreports.cyb.no and foreningenbs.no/okoreports
 
-# deploy script - deploys to internt.cyb.no/okoreports and foreningenbs.no/okoreports
-
-echo "Deploying to internt.cyb.no"
-ssh django@internt.cyb.no /bin/bash <<EOF
-  cd /home/django/okotools
-  git pull
-  cd tripletexweb/web
-  npm install
-  npm run build
-  cd ../../tripletex
-  . env/bin/activate
-  pip install -r requirements.txt
+echo "Deploying to okoreports.cyb.no"
+ssh root@okoreports.cyb.no /bin/bash <<EOF
+  cd /root/drift/okoreports
+  ./update.sh
 EOF
 
 echo
 echo "Deploying to foreningenbs.no"
 ssh henrste@foreningenbs.no /bin/bash <<EOF
+  set -e
   eval \$(ssh-agent)
   ssh-add ~/.ssh/cyb_okotools
   cd /var/www/aliases/okoreports/okotools
   git pull
-  cd tripletexweb/web
-  npm install
-  npm run build
-  cd ../../tripletex
-  . env/bin/activate
-  pip install -r requirements.txt
+  cd tripletexweb/backend
+  ./update.sh
+  cd ../frontend
+  ./update-dist.sh
 EOF
