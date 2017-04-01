1ac3120b737de47574c26ba1e9db873b98022052
Henrik Steen <henrist@henrist.net>
2017-04-01 18:17:20 +0200

    Decouple building and using docker image

--- tripletexweb/backend/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# we need the tripletex-folder as well, so copy this in and remove later
-cp -rp ../../tripletex tripletex-lib
-
-commit=$(git rev-parse --short HEAD)
-docker build -t cyb/okoreports-backend:$commit -t cyb/okoreports-backend:latest .
-
-rm -rf tripletex-lib
--- tripletexweb/backend/update.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-./build.sh
-
-docker rm -f cyb-okoreports-backend 2>/dev/null || true
-
-./run.sh
