ef111a255d35964649961ad2eb2a86141bcface1
Henrik Steen <henrist@henrist.net>
2017-04-01 22:14:51 +0200

    Update deploy script

--- tripletexweb/deploy.sh
+++ tripletexweb/deploy.sh
@@ -4,20 +4,17 @@
 
 echo "Deploying to okoreports.cyb.no"
 ssh root@okoreports.cyb.no /bin/bash <<EOF
+  set -e
   cd /root/drift/okoreports
-  ./update.sh
+  docker-compose pull
+  docker-compose up -d
 EOF
 
 echo
 echo "Deploying to foreningenbs.no"
 ssh henrste@foreningenbs.no /bin/bash <<EOF
   set -e
-  eval \$(ssh-agent)
-  ssh-add ~/.ssh/cyb_okotools
-  cd /var/www/aliases/okoreports/okotools
-  git pull
-  cd tripletexweb/backend
-  ./update.sh
-  cd ../frontend
-  ./update-dist.sh
+  cd /fbs/drift/services/okoreports
+  docker-compose pull
+  docker-compose up -d
 EOF
