4fb8f76246c2d81fa9ab7b831b5fa8ab7fd89818
Henrik Steen <henrist@henrist.net>
2017-04-01 23:59:00 +0200

    tripletex-importer: Use docker-compose

--- tripletex-importer/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# we need the tripletex-folder as well, so copy this in and remove later
-cp -rp ../tripletex tripletex-lib
-
-docker build -t cyb/tripletex-importer .
-
-rm -rf tripletex-lib
