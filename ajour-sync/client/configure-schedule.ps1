b7a263796dd60680df96ee49fb98f9bea298ba56
Henrik Steen <henrist@henrist.net>
2020-01-03 23:02:30 +0100

    schedule

---
+++ ajour-sync/client/configure-schedule.ps1
@@ -0,0 +1,10 @@
+# Set it up to run every second hour.
+
+# See https://www.reddit.com/r/PowerShell/comments/8vjpzq/registerscheduledtask_using_daily_and_repetition/e1nwghk/
+
+$a = New-ScheduledTaskAction -Execute "C:\cyboko\sync.bat"
+$tr = New-ScheduledTaskTrigger -Daily -At '00:30'
+$t = Register-ScheduledTask -TaskName "CYB Ajour Sync" -Trigger $tr -Action $a -User "SHUTTLE\Ajour"
+$t.Triggers.Repetition.Duration = 'P7300D'
+$t.Triggers.Repetition.Interval = 'PT120M'
+$t | Set-ScheduledTask
